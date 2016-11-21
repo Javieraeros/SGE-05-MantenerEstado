e9d42e391153926a8fd0630b30f8895655ffe89a
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2016-11-21 11:58:06 +0100

    Por ahora nada más

---
+++ Sessions/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sessions.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost,ActionName("Index")]
+        public ActionResult IndexPost(string boton,string txtNombre)
+        {
+            switch (boton)
+            {
+                case "Log In":
+                    Session["usuario"] =Request.Form["txtNombre"];
+
+                    Session["usuario"] = txtNombre;
+                    break;
+                case "+ 1":
+                    Session["contador"]=Convert.ToInt32(Session["contador"])+1;
+                    break;
+
+                case "Cerrar Sesion":
+                    Session.Clear();
+                    Session.Abandon();
+                    break;
+            }
+            return View();
+        }
+    }
+}
